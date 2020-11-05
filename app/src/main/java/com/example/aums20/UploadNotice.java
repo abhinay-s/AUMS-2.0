ff289c8c66f7d52cdaf2f061cc1fb0092904cbd8
abhinay-s <abhinay0498@gmail.com>
2020-11-05 14:02:26 +0530

    Successfully Added and stored Teacher's data in the database

--- app/src/main/java/com/example/aums20/UploadNotice.java
+++ app/src/main/java/com/example/aums20/UploadNotice.java
@@ -42,7 +42,7 @@ public class UploadNotice extends AppCompatActivity {
 
     private final int REQ=1;
     private Bitmap bitmap;
-    private DatabaseReference reference;
+    private DatabaseReference reference, dbRef;
     private StorageReference storageReference;
     String downloadUrl="";
     private ProgressDialog pd;
@@ -117,8 +117,8 @@ public class UploadNotice extends AppCompatActivity {
     }
 
     private void uploadData() {
-        reference=reference.child("Notice");
-        final String uniqueKey=reference.push().getKey();
+        dbRef=reference.child("Notice");
+        final String uniqueKey=dbRef.push().getKey();
 
         String title=noticeTitle.getText().toString();
 
@@ -132,7 +132,7 @@ public class UploadNotice extends AppCompatActivity {
 
         NoticeData noticeData=new NoticeData(title,downloadUrl,date,time,uniqueKey);
 
-        reference.child(uniqueKey).setValue(noticeData).addOnSuccessListener(new OnSuccessListener<Void>() {
+        dbRef.child(uniqueKey).setValue(noticeData).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void aVoid) {
                 pd.dismiss();
